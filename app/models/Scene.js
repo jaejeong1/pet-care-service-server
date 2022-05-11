ac143d3b8e89faf59a77a0c2873003b28205f8fb
JaeJeong <wowjd555@gmail.com>
2022-05-11 16:30:40 +0900

    Scene type 오류 수정 및 .env 파일제외(환경변수는 각자 로컬에서 관리)

--- app/models/Scene.js
+++ app/models/Scene.js
@@ -1,6 +1,6 @@
 export default function SceneModel(mongoose) {
     const sceneSchema = mongoose.Schema({
-        index: {type: Integer, unique: 1},
+        index: {type: Number, unique: 1},
         content: String
     })
     return mongoose.model('Scene', sceneSchema)
