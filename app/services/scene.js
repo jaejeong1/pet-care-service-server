803c3d5709081f7db9a25274cd9aa493e0f7346d
JaeJeong <wowjd555@gmail.com>
2022-05-12 01:55:17 +0900

    Scene API 구현

--- app/services/scene.js
+++ app/services/scene.js
@@ -1,14 +1,28 @@
 import db from '../modles/index.js'
 import getDatabase from '../lambdas/getDatabase.js'
+import { http } from 'winston'
+import request from 'request'
 
 export default function SceneService() {
 
     const Scene = db.Scene
     const dbo = getDatabase()
     const dbConnect = dbo.getDb()
+    const clientToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImFpc3R1ZGlvcy5jb20iLCJwbGF0Zm9ybSI6IndlYiIsImlhdCI6MTY1MjI3OTM0MCwiZXhwIjoxNjUyMjc5NjQwfQ.kn8LTgw_vTtOGxLM0oKRhcUDTio3wUnC2zn95iwM0s8"
+    const appId = "aistudios.com"
+    const baseUrl = "https://dev.aistudios.com/api/odin/editor/scene"
 
     return {
         add(req, res) {
+            // 1. Scene 추가
+            request.post(baseUrl + "/key", {form:{screenIdx:req.body.index, scene:req.body.context}}, function(err, httpResponse, body) {
+                res
+                    .status(500)
+                    .send({message: err});
+                console.log('씬 추가 실패')
+                return;
+            })
+            // 2. 데이터 DB 저장
             new Scene(req.body).save(function (err) {
                 if (err) {
                     res
@@ -24,9 +38,25 @@ export default function SceneService() {
             })
         },
         delete(req, res) {
-            
+            // 1. Scene 삭제
+            request.delete(baseUrl + "/key", {form:{screenIdx:req.body.index}}, function(err, httpResponse, body) {
+                res
+                    .status(500)
+                    .send({message: err});
+                console.log('씬 추가 실패')
+                return;
+            })
         },
         update(req, res) {
+            // 1. Scene 변경
+            request.put(baseUrl + "/key", {form:{screenIdx:req.body.index, scene:req.body.context}}, function(err, httpResponse, body) {
+                res
+                    .status(500)
+                    .send({message: err});
+                console.log('씬 추가 실패')
+                return;
+            })
+
             Scene.findOneandUpdate({
                 index: req.body.index
             }, function (err, user) {
@@ -37,7 +67,7 @@ export default function SceneService() {
                         .status(401)
                         .send({success: false, message: '해당 INDEX가 존재하지 않습니다'})
                 } else {
-                    
+
                 }
             })
         }
